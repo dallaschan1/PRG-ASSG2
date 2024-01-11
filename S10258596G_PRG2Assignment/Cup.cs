2c9d94251105aa71b527ef960b1d5e52e9b4c4b4
Ian <iancheif117@gmail.com>
2024-01-12 00:09:23 +0800

    Completed all the classes on the right

--- S10258596G_PRG2Assignment/Cup.cs
+++ S10258596G_PRG2Assignment/Cup.cs
@@ -10,11 +10,46 @@ namespace Assignment2
     {
         public Cup() { }
 
-        public Cup(string option, int scoops, List<Flavour> flavours, List<Topping> toppings) : base(option, scoops, flavours, toppings) { }
+        public Cup(string option, int scoops, List<Flavour> flavours, List<Topping> toppings) : base("Cup", scoops, flavours, toppings) { }
 
-        public double CalculatePrice()
+        public override double CalculatePrice()
         {
+            double singleScopp = 4.00;
+            double doubleScopp = 5.50;
+            double threeScoop = 6.50;
+            double totalPrice = 0.00;
 
+            // Adding Price depending on the number of scoops ordered
+            switch (Scoops)
+            {
+                case 1:
+                    totalPrice += singleScopp;
+                    break;
+                case 2:
+                    totalPrice += doubleScopp;
+                    break;
+                case 3:
+                    totalPrice -= threeScoop;
+                    break;
+            }
+
+            // Adding any additional costs if there are any premium flavours
+            double totalFlavourPrice = 0.00;
+            foreach (Flavour flavour in Flavours)
+            {
+                if (flavour.Premium)
+                {
+                    totalFlavourPrice += 2.00;
+                }
+            }
+            totalPrice += totalFlavourPrice;
+
+            // Adding any additional topping costs if there are any premium toppings
+            double toppingPrice = Toppings.Count() * 1.00;
+            totalPrice += toppingPrice;
+
+
+            return totalPrice;
         }
 
         public override string ToString()
